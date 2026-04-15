82a8d55f5db1fccff6a2e0a86857ef5079a04517
Santiagoforero <santiagoforerorodriguez1@gmail.com>
2026-04-14 20:04:37 -0500

    supabase2

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,5 @@
 import { Button } from '@/components/ui/button'
-import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+
 import { useAuth } from '@/context/AuthContext'
 import { Link, Navigate } from 'react-router-dom'
 
